6b7fa39c462b3d900c1b6d3fc32ea41dda4e366f
Lucas Nithael <94084548+LucasNithael@users.noreply.github.com>
2022-06-30 14:06:04 +0000

    lista 08

--- Lista07POO/3Paciente.cs
+++ Lista07POO/3Paciente.cs
@@ -6,10 +6,10 @@ class Program{
   public static void Main(){
     CultureInfo ci = new CultureInfo("pt-BR");
     Thread.CurrentThread.CurrentCulture = ci;
-    Paciente p = new Paciente{Nome = "matheus", CPF = "70541519417", Telefone = "40028922", Nascimento = DateTime.Parse("27/7/2025")};
+    Paciente p = new Paciente{Nome = "matheus", CPF = "70541519417", Telefone = "40028922", Nascimento = DateTime.Parse("04/04/2000")};
     
     Console.WriteLine(p);
-    Console.WriteLine(p.Idade);
+    Console.WriteLine(p.Idade());
   }
 }
 
@@ -29,8 +29,15 @@ class Paciente{
     get {return nascimento;}
     set {nascimento = value;}
   }
-  public string Idade{
-    get => $"{DateTime.Today.Year - nascimento.Year} anos e {Math.Abs(DateTime.Today.Month - nascimento.Month)} meses";
+  public string Idade(){
+    DateTime hoje = DateTime.Today;
+    int ano = hoje.Year - nascimento.Year;
+    int mes = hoje.Month - nascimento.Month;
+    if(mes<0){
+      mes = mes + 12;
+      ano--;
+    }
+    return $"{ano} ano(s) e {mes} mes(es)";
   }
   public override string ToString(){
     return $"{nome} - {cpf} - {telefone} - {nascimento: dd/MM/yyyy}";
--- Lista09POO/1MediaDisciplinas.cs
+++ Lista09POO/1MediaDisciplinas.cs
@@ -3,6 +3,14 @@ using System.Collections;
 
 class MainClass{
   public static void Main(){
+    IDisciplina d1 = new DisciplinasAnual(30, 40, 50, 60, 70);
+    IDisciplina d2 = new DisciplinasAnual(20, 10, 80, 65, 60);
+    //IDisciplina d = new DisciplinasSemestral(30, 70);
+    Historico h = new Historico();
+    h.InserirDisciplinasAnual(d1);
+    h.InserirDisciplinasAnual(d2);
+    h.ListarDisciplinasAnual();
+    
     
   }
 }
@@ -14,10 +22,39 @@ interface IDisciplina{
   int CalcMediaFinal();
 }
 
+class Historico{
+  private DisciplinasAnual[] disciplinasAnual = new DisciplinasAnual[1];
+ private DisciplinasSemestral[] disciplinasSemestral = new DisciplinasSemestral[1];
+  private int k1;
+  private int k2;
+  public void InserirDisciplinasAnual(DisciplinasAnual d1){
+    if(k1==disciplinasAnual.Length)
+      Array.Resize(ref disciplinasAnual, 1 + disciplinasAnual.Length);
+    disciplinasAnual[k1] = d;
+    k1++;
+  }
+  public void InserirDisciplinasSemestral(DisciplinasSemestral d2){
+    if(k2==disciplinasSemestral.Length)
+      Array.Resize(ref disciplinasSemestral, 1 + disciplinasSemestral.Length);
+    disciplinasSemestral[k2] = d;
+    k2++;
+  }
+  public void ListarDisciplinasSemestral(){
+    foreach(IDisciplina d in disciplinasSemestral)
+      if(d!=null)
+        Console.WriteLine($"{d.GetNome()} - {d.CalcMediaFinal()}");
+  }
+  public void ListarDisciplinasAnual(){
+    foreach(IDisciplina d in disciplinasSemestral)
+      Console.WriteLine($"{d.GetNome()} - {d.CalcMediaFinal()}");
+  }
+
+}
+
 class DisciplinasAnual : IDisciplina{
   private string nome;
   private int nota1, nota2, nota3, nota4, notaFinal;
-  public DisciplinaAnual(int n1, int n2, int n3, int n4, int nf){
+  public DisciplinasAnual(int n1, int n2, int n3, int n4, int nf){
     nota1 = n1; nota2 = n2; nota3 = n3; nota4 = n4; notaFinal = nf;
   }
   public string GetNome(){ return nome;}
@@ -34,7 +71,7 @@ class DisciplinasSemestral : IDisciplina{
   private int nota1, nota2, notaFinal;
   public string GetNome(){ return nome;}
   public int CalcMediaParcial(){
-    return (nota1+nota2+nota3+nota4)/4;
+    return (nota1+nota2)/2;
   }
   public int CalcMediaFinal(){
     return (CalcMediaParcial()+notaFinal)/2;
